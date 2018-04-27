a9508ce9fefea045cb1e562db0c922a8285aaefa
nrivera <bloodthune@gmail.com>
2018-04-27 13:30:29 -0400

    Basic applicaiton structure setup

---
+++ src/ars.engine/ArsEngine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ars.engine
+{
+    internal class ArsEngine
+    {
+        internal bool StartService()
+        {
+            return true;
+        }
+
+        internal bool StopService()
+        {
+            return true;
+        }
+    }
+}
--- src/ars.engine/Program.cs
+++ src/ars.engine/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Serilog;
+using System;
+using System.Diagnostics;
+using System.IO;
+using Topshelf;
+using System.Configuration;
+
 
 namespace ars.engine
 {
@@ -10,6 +12,44 @@ namespace ars.engine
     {
         static void Main(string[] args)
         {
+            Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
+            Serilog.Debugging.SelfLog.Enable(s => Trace.WriteLine($"SERILOG ERROR: {s}"));
+
+            if (Convert.ToBoolean(ConfigurationManager.AppSettings["useLogFile"]))
+            {
+                Log.Logger = new LoggerConfiguration()
+                    .ReadFrom.AppSettings()
+                    .WriteTo.RollingFile(@".\Logs\log-{Date}.txt")
+                    .Enrich.FromLogContext()
+                    .CreateLogger();
+            }
+            else
+            {
+                Log.Logger = new LoggerConfiguration()
+                    .ReadFrom.AppSettings()
+                    .WriteTo.EventLog(ConfigurationManager.AppSettings["appName"], manageEventSource: true)
+                    .WriteTo.ColoredConsole()
+                    .Enrich.FromLogContext()
+                    .CreateLogger();
+            }
+
+            HostFactory.Run(x =>
+            {
+                x.UseSerilog();
+                x.Service<ArsEngine>(s =>
+                {
+                    s.ConstructUsing(name => new ArsEngine());
+                    s.WhenStarted(tc => tc.StartService());
+                    s.WhenStopped(tc => tc.StopService());
+                });
+
+                x.UseLinuxIfAvailable();
+                x.StartAutomatically();
+                x.RunAsLocalSystem();
+                x.SetDescription(ConfigurationManager.AppSettings["appDesc"]);
+                x.SetDisplayName(ConfigurationManager.AppSettings["appName"]);
+                x.SetServiceName(ConfigurationManager.AppSettings["appServiceName"]);
+            });
         }
     }
 }
